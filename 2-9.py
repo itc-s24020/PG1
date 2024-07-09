9dc711ae43cd82facb37ddc6f8880894286f1109
itc-s24020 <s24020@std.it-college.ac.jp>
2024-07-09 06:35:37 +0000

    2-9.pyおわり

---
+++ 2-9.py
@@ -0,0 +1,11 @@
+if True:
+    print('True')
+else:
+    print('False')
+x = 3
+print(x>3)
+my_list = [0, 2, 4, 6, 8, 10]
+print(3 in my_list)
+my_list = [0, 2, 4, 6, 8, 10]
+print(3 not in my_list)
+
