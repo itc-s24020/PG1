65d91f45f461119c4ab0dae837cf7bf03a61fabc
itc-s24020 <s24020@std.it-college.ac.jp>
2024-08-06 07:03:54 +0000

    6-1.pyおわり

---
+++ 6-1.py
@@ -0,0 +1,24 @@
+class Car:
+    weight = 4000
+    num_wheels = 4
+
+    def __init__(self, car_name='NoName'):
+        self.name = car_name
+
+    def calc_weight_per_wheel(self):
+        return self. weight / self.num_wheels
+
+default_car = Car()
+
+print(default_car.name)
+my_car = Car('DeLorean')
+print(my_car.name)
+
+class Cat:
+    cry = "にゃー"
+    legs = 4
+    is_animal = True
+
+tama = Cat()
+print("鳴き声:{},　足の数:{}, 動物:{}".format(tama.cry, tama.legs, tama.is_animal))
+
